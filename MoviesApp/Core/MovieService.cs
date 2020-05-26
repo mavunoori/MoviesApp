d8d0b8b61633101907f3126ad7bf75962ef08cc4
Madhukar <53446053+mavunoori@users.noreply.github.com>
2020-05-26 18:54:55 +0530

    Update MovieService.cs

--- MoviesApp/Core/MovieService.cs
+++ MoviesApp/Core/MovieService.cs
@@ -11,7 +11,7 @@ namespace MoviesApp.Core
         {
             return new List<Movie>()
                 {
-                new Movie{ ID=1,Name="Movie1"},
+                new Movie { ID = 1, Name = "Movie1"},
                 new Movie { ID = 2, Name = "Movie2" },
                 new Movie { ID = 3, Name = "Movie3" },
                 new Movie { ID = 4, Name = "Movie4" }
